e052cca732902fff8e887e034a94c192df6daee5
violette <violette.roche-montane@forgerock.com>
2016-02-24 16:06:07 +0100

    Fixup style

--- Examples/OPENIG-836/.openig/scripts/groovy/display_if_401.groovy
+++ Examples/OPENIG-836/.openig/scripts/groovy/display_if_401.groovy
@@ -7,18 +7,21 @@ import org.forgerock.http.protocol.Status
 
 // This script displays in the console the advices if they are present.
 next.handle(context, request)
-    .thenOnResult { if (it.status == UNAUTHORIZED) {
+    .thenOnResult {
+                  if (it.status == UNAUTHORIZED) {
                        println("(DEBUG) Script >>>> " + attributes.myPolicyDecision)
                        it.status = Status.OK
-                       it.entity = new ByteArrayBranchingStream(("<html> \
-                                                <head><title id='head'>Policy UNAUTHORIZED access the protected resource!</title> \
-                                                <link rel='shortcuticon'href='http: //forgerock.com/favicon.ico'> \
-                                                <link rel='icon' type='image/png' href='https://forgerock.org/ico/favicon-196x196.png' /> \
-                                                </head> \
-                                                <p> UNAUTHORIZED access to the resource for <b>" + attributes.user + "</b> \
-                                                <p> Extra attributes in context: <b>" + attributes.myPolicyDecision + "</b></p>").getBytes())              
+                       it.entity = new ByteArrayBranchingStream((
+                           """<html>
+                              <head><title id='head'>Policy UNAUTHORIZED access the protected resource!</title>
+                              <link rel='shortcuticon'href='http: //forgerock.com/favicon.ico'>
+                              <link rel='icon' type='image/png' href='https://forgerock.org/ico/favicon-196x196.png' />
+                              </head>
+                              <p> UNAUTHORIZED access to the resource for <b>${attributes.user}</b>
+                              <p> Extra attributes in context: <b>${attributes.myPolicyDecision}</b></p>
+                           """).getBytes())
                         return it
                     } else {
                       return it
-                    }       
+                    }
                   } 
