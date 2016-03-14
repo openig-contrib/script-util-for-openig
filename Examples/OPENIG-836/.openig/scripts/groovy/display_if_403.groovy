c933af19c5eefa759c3786e19387115c3311a1df
violette <violette.roche-montane@forgerock.com>
2016-03-14 22:52:56 +0100

    Update due to OPENIG-871
    The policy enforcement filter returns a 403 when the policy denies
    access to the requested resource.

--- Examples/OPENIG-836/.openig/scripts/groovy/display_if_403.groovy
+++ Examples/OPENIG-836/.openig/scripts/groovy/display_if_403.groovy
@@ -1,4 +1,4 @@
-import static org.forgerock.http.protocol.Status.UNAUTHORIZED
+import static org.forgerock.http.protocol.Status.FORBIDDEN
 
 import org.forgerock.http.io.ByteArrayBranchingStream
 import org.forgerock.http.protocol.Response;
@@ -8,16 +8,16 @@ import org.forgerock.http.protocol.Status
 // This script displays in the console the advices if they are present.
 next.handle(context, request)
     .thenOnResult {
-                  if (it.status == UNAUTHORIZED) {
+                  if (it.status == FORBIDDEN) {
                        println("(DEBUG) Script >>>> " + attributes.myPolicyDecision)
                        it.status = Status.OK
                        it.entity = new ByteArrayBranchingStream((
                            """<html>
-                              <head><title id='head'>Policy UNAUTHORIZED access the protected resource!</title>
+                              <head><title id='head'>Policy FORBIDDEN access the protected resource!</title>
                               <link rel='shortcuticon'href='http: //forgerock.com/favicon.ico'>
                               <link rel='icon' type='image/png' href='https://forgerock.org/ico/favicon-196x196.png' />
                               </head>
-                              <p> UNAUTHORIZED access to the resource for <b>${attributes.user}</b>
+                              <p> FORBIDDEN access to the resource for <b>${attributes.user}</b>
                               <p> Extra attributes in context: <b>${attributes.myPolicyDecision}</b></p>
                            """).getBytes())
                         return it
