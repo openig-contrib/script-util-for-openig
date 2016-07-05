6119c6453ef888470a78a8935042458cd3517988
violette <violette.roche-montane@forgerock.com>
2016-07-05 10:46:27 +0200

    Fix for groovy script
    The thenOnResult is no longer needed and has been replaced by a simple then().

--- PEP/OPENIG-836/.openig/scripts/groovy/display_if_403.groovy
+++ PEP/OPENIG-836/.openig/scripts/groovy/display_if_403.groovy
@@ -2,23 +2,21 @@ import static org.forgerock.http.protocol.Status.FORBIDDEN
 
 import org.forgerock.http.io.ByteArrayBranchingStream
 
-
-// This script displays in the console the advices if they are present.
+// This script manage the 403 - FORBIDDEN response when the policy decision denies the request:
 next.handle(context, request)
-    .thenOnResult {
-                  if (it.status == FORBIDDEN) {
-                       it.status = Status.OK
-                       it.entity = new ByteArrayBranchingStream((
-                           """<html>
-                              <head><title id='head'>Policy FORBIDDEN access the protected resource!</title>
-                              <link rel='shortcuticon'href='http: //forgerock.com/favicon.ico'>
-                              <link rel='icon' type='image/png' href='https://forgerock.org/ico/favicon-196x196.png' />
-                              </head>
-                              <p> FORBIDDEN access to the resource for <b>${attributes.user}</b>
-                              <p> Extra attributes in context: <b>${attributes.myPolicyDecision}</b></p>
-                           """).getBytes())
-                        return it
-                    } else {
-                      return it
-                    }
-                  }
+        .then {if (it.status == FORBIDDEN) {
+                   it.status = Status.OK
+                   it.entity = new ByteArrayBranchingStream((
+                    """<html>
+					  <head><title id='head'>Policy FORBIDDEN access the protected resource!</title>
+					    <link rel='shortcuticon'href='http: //forgerock.com/favicon.ico'>
+					    <link rel='icon' type='image/png' href='https://forgerock.org/ico/favicon-196x196.png' />
+					  </head>
+					  <p><b>${attributes.user}</b>, your access to this ressource has been limited.</p>
+					  <br/>
+					  <p><h3>TIP:</h3> This ressource is available only on:
+					  ${attributes.myPolicyDecision.advices.VALID_DAYS_OF_THE_WEEK}</p>
+				   """).getBytes())
+}
+    return it
+}
