e96e25d96a43e63044002fd371db7ffabba4c16e
violette <violette.roche-montane@forgerock.com>
2016-08-26 14:11:24 +0200

    Configuration examples for AWS scripts

---
+++ AWS-config-examples/hello-world.groovy
@@ -0,0 +1,4 @@
+Response response = new Response(Status.OK)
+String localhostname = java.net.InetAddress.getLocalHost().getHostName();
+response.entity = "Hello World! from ${localhostname}" as String
+return response
